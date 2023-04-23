260c38addc20e8b16fc657ff90adf271ee003821
vkso <yuchenv@qq.com>
2023-04-23 11:40:09 +0800

    员工管理页面开发完成。

--- src/main/java/com/home/reggie/controller/EmployeeController.java
+++ src/main/java/com/home/reggie/controller/EmployeeController.java
@@ -1,16 +1,15 @@
 package com.home.reggie.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.home.reggie.common.R;
 import com.home.reggie.entity.Employee;
 import com.home.reggie.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
@@ -97,7 +96,66 @@ public class EmployeeController {
         return R.success("新增员工成功");
 
     }
+
+    /**
+     * 员工信息分页查询
+     * @param page
+     * @param pageSize
+     * @param name
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize, String name) {
+        log.info("page = {}, pageSize = {}, name = {}", page, pageSize, name);
+
+        // 构造分页构造器
+        Page pageInfo = new Page(page, pageSize);
+
+        // 条件构造器（name）
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper();
+        queryWrapper.like(StringUtils.isNotEmpty(name), Employee::getName, name);
+        // 排序条件
+        queryWrapper.orderByDesc(Employee::getUpdateTime);
+
+        // 执行查询
+        employeeService.page(pageInfo, queryWrapper);
+
+        return R.success(pageInfo);
+    }
+
+    /**
+     * 根据 ID 修改员工信息
+     * @param employee
+     * @return
+     */
+    @PutMapping
+    public R<String> update(HttpServletRequest request, @RequestBody Employee employee) {
+        log.info(employee.toString());
+
+        employee.setUpdateTime(LocalDateTime.now());
+        Long empID = (Long) request.getSession().getAttribute("employee");
+        employee.setUpdateUser(empID);
+        employeeService.updateById(employee);
+        return R.success("员工信息修改成功");
+    }
+
+    /**
+     * 根据 id 查询员工信息
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    public R<Employee> getById(@PathVariable Long id) {
+
+        log.info("根据id查询员工信息..., id = {}", id);
+        Employee employee = employeeService.getById(id);
+        if (null != employee) {
+            return R.success(employee);
+        }
+        return R.error("没有查询到对应的员工信息");
+    }
 }
+
 
 
 
