d9e097682dc293e64747369a8081fa21f19302ce
vkso <yuchenv@qq.com>
2023-04-23 09:47:58 +0800

    新增员工功能完成，并处理了新增员工信息 username重复的异常。

--- src/main/java/com/home/reggie/controller/EmployeeController.java
+++ src/main/java/com/home/reggie/controller/EmployeeController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 @Slf4j
 @RestController
@@ -77,9 +78,24 @@ public class EmployeeController {
      * @param employee
      * @return
      */
-    public R<String> save(@RequestBody Employee employee) {
+    @PostMapping
+    public R<String> save(HttpServletRequest request, @RequestBody Employee employee) {
         log.info("新增员工，员工信息：{}", employee.toString());
-        return null;
+
+        // 设置初始密码：123456，需要使用md5加密
+        employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
+
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+
+        // 获取当前用户登录的 id
+        Long empId = (Long) request.getSession().getAttribute("employee");
+        employee.setCreateUser(empId);
+        employee.setUpdateUser(empId);
+
+        employeeService.save(employee);
+        return R.success("新增员工成功");
+
     }
 }
 
