6d94a00677f278816f2e8830067981b54b26aee8
vkso <yuchenv@qq.com>
2023-04-23 22:09:16 +0800

    文件上传、下载功能开发完成。

--- src/main/java/com/home/reggie/filter/LoginCheckFilter.java
+++ src/main/java/com/home/reggie/filter/LoginCheckFilter.java
@@ -36,7 +36,8 @@ public class LoginCheckFilter implements Filter {
                 "/employee/login",
                 "/employee/logout",
                 "/backend/**",
-                "/front/**"
+                "/front/**",
+                "/common/**"
         };
 
         // 2. 判断本次请求是否需要处理(使用类中的 check 方法进行匹配)
